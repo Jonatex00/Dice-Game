caa7dc878a6fdae3a09ca6128e11e6f8d95cfcca
Jonatex00 <jonathanjohnonwu@gmail.com>
2024-03-28 02:20:17 +0100

    Special features added

--- script.js
+++ script.js
@@ -5,29 +5,22 @@ let user2 = '';
 
 const startGame = () => {
   selectors.user0El.textContent = user1;
-  selectors.user1El.textContent = user2;
+  loadScoresFromLocalStorage();
   init();
 };
 
 const checkAndStartGame = event => {
   event.preventDefault();
   user1 = selectors.firstUserInput.value;
-  user2 = selectors.secondUserInput.value;
 
-  const containNonSpaceChar = /\S/.test(user1) && /\S/.test(user2);
-  if (
-    user1 !== '' &&
-    user2 !== '' &&
-    containNonSpaceChar &&
-    isNaN(user1) &&
-    isNaN(user2)
-  ) {
+  const containNonSpaceChar = /\S/.test(user1);
+  if (user1 !== '' && containNonSpaceChar && isNaN(user1)) {
     startGame();
     setTimeout(() => {
       selectors.startPlayingHeader.style.display = 'none';
     }, 2000);
   } else {
-    alert("Please enter both players' names to start the game.");
+    alert('Please enter players names to start the game.');
   }
 };
 selectors.startButton.addEventListener('click', checkAndStartGame);
@@ -41,6 +34,7 @@ const startTimer = function () {
 
 const endGame = function () {
   clearInterval(timeInterval);
+  saveScoresToLocalStorage();
 };
 
 const updateTimer = function () {
@@ -85,15 +79,23 @@ const declareWinner = function (winner) {
   } else {
     user2Score++;
   }
-
-  // Display updated scores
-  selectors.user1ScoreDisplay.textContent = `${user1Score}:`;
-  selectors.user2ScoreDisplay.textContent = `${user2Score}`;
+  updateScoresDisplay();
 };
+// Display updated scores
+const updateScoresDisplay = function () {
+  selectors.user1ScoreDisplay.textContent = `${user1Score}`;
+  selectors.user2ScoreDisplay.textContent = `${user2Score}`;
+  updateScoresAndSaveToLocalStorage();
+};
+
+window.addEventListener('load', function () {
+  loadScoresFromLocalStorage();
+});
+
 selectors.form.classList.remove('hidden');
 let scores, currentScore, activePlayer, playing;
 playing = false;
-selectors.main.classList.add('d-hidden');
+
 //Starting conditions
 const init = function () {
   scores = [0, 0];
@@ -105,6 +107,8 @@ const init = function () {
   selectors.score1El.textContent = 0;
   selectors.current0El.textContent = 0;
   selectors.current1El.textContent = 0;
+  selectors.user1ScoreDisplay.textContent = `${user1Score}`;
+  selectors.user2ScoreDisplay.textContent = `${user2Score}`;
   selectors.form.classList.add('hidden');
   selectors.startPlayingHeader.classList.remove('hidden');
   selectors.diceEl.classList.add('hidden');
@@ -117,6 +121,7 @@ const init = function () {
   selectors.player0El.classList.add('player--active');
   selectors.player1El.classList.remove('player--active');
   startTimer();
+  updateScoresDisplay();
 };
 // ====== Active Player ======
 const switchPlayer = function () {
@@ -141,21 +146,62 @@ selectors.btnRoll.addEventListener('click', function () {
         currentScore;
     } else {
       switchPlayer();
+      computerLogic();
     }
   }
 });
 
+const computerLogic = function () {
+  if (playing && activePlayer === 1) {
+    const thresholds = [7, 12, 9, 15, 10, 22, 8];
+    const randomIndex = Math.floor(Math.random() * thresholds.length);
+    const threshold = thresholds[randomIndex];
+    setTimeout(() => {
+      const dice = Math.trunc(Math.random() * 6) + 1;
+      selectors.diceEl.classList.remove('hidden');
+      selectors.diceEl.src = `dice-${dice}.png`;
+      selectors.diceSound.play();
+
+      if (dice !== 1) {
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent =
+          currentScore;
+        if (currentScore < threshold) {
+          computerLogic(); // Continue rolling if below threshold
+        } else {
+          // Hold the current score
+          scores[activePlayer] += currentScore;
+          document.getElementById(`score--${activePlayer}`).textContent =
+            scores[activePlayer];
+          if (scores[activePlayer] >= 50) {
+            playing = false; // End the game
+            selectors.diceEl.classList.add('hidden');
+            selectors.winnerSound.play();
+            document
+              .querySelector(`.player--${activePlayer}`)
+              .classList.add('player--winner');
+            selectors.winner.classList.remove('hidden');
+            declareWinner(); // Display winner
+            endGame();
+          } else {
+            switchPlayer(); // Switch to the next player
+          }
+        }
+      } else {
+        switchPlayer();
+      }
+    }, 1000);
+  }
+};
+
 selectors.btnHold.addEventListener('click', function () {
-  if (playing) {
-    selectors.holdSound.play();
+  if (playing && activePlayer === 0) {
     // 1. Add current score to Active player's score
     scores[activePlayer] += currentScore;
-    // scores[1] = scores[1] + currentScore
-
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    //2. Check if player's score is >= 100
+    //2. Check if player's score is >= 20
     if (scores[activePlayer] >= 50) {
       // Finish the game
       playing = false;
@@ -173,16 +219,50 @@ selectors.btnHold.addEventListener('click', function () {
     } else {
       //Switch to the next player
       switchPlayer();
+      // Execute computer logic after human player's turn
+      computerLogic();
     }
   }
 });
 
 selectors.btnNew.addEventListener('click', function () {
-  if (user1 !== '' && user2 !== '') {
+  if (user1 !== '') {
     document.body.style.backgroundImage =
       'linear-gradient(to top left, #753682 0%, #bf2e34 100%)';
     init();
   } else {
-    alert("Please enter both players' names to start the game.");
+    alert('Please enter players names to start the game.');
   }
+});
+
+// Function to save scores to local storage
+const saveScoresToLocalStorage = function () {
+  localStorage.setItem('user1Score', JSON.stringify(user1Score));
+  localStorage.setItem('user2Score', JSON.stringify(user2Score));
+};
+
+// Function to load scores from local storage
+const loadScoresFromLocalStorage = function () {
+  const storedUser1Score = JSON.parse(localStorage.getItem('user1Score'));
+  const storedUser2Score = JSON.parse(localStorage.getItem('user2Score'));
+  if (storedUser1Score !== null) user1Score = storedUser1Score;
+  if (storedUser2Score !== null) user2Score = storedUser2Score;
+  if (storedUser1Score !== null || storedUser2Score !== null) {
+    updateScoresDisplay();
+  }
+};
+const updateScoresAndSaveToLocalStorage = function () {
+  saveScoresToLocalStorage();
+};
+
+const clearLocalStorage = function () {
+  localStorage.removeItem('user1Score');
+  localStorage.removeItem('user2Score');
+  user1Score = 0;
+  user2Score = 0;
+  selectors.user1ScoreDisplay.textContent = '0';
+  selectors.user2ScoreDisplay.textContent = '0';
+};
+selectors.resetScore.addEventListener('click', function () {
+  clearLocalStorage();
 });
